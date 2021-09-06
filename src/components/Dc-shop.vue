ff05447d82366fa6f200ebe292c8adc278601b19
Giorgio Andrei <cipuandrei@libero.it>
2021-09-06 23:53:50 +0200

    shop-list-dinamically-displayed

--- src/components/Dc-shop.vue
+++ src/components/Dc-shop.vue
@@ -2,17 +2,58 @@
   <section>
     <div class="container h-100">
       <ul class="h-100">
-        <li><a href="#">Digital comics</a></li>
-        <li><a href="#">DC merchandise</a></li>
+        <li v-for="(link, index) in links" :key="index">
+          <a :href="link.href"
+            ><img :src="link.icon" :alt="`buy-${link.text}`" /><span>{{
+              link.text
+            }}</span></a
+          >
+        </li>
+
+        <!--         <li><a href="#">DC merchandise</a></li>
         <li><a href="#">Subscription</a></li>
         <li><a href="#">Comic shop locator</a></li>
-        <li><a href="#">Dc power visa</a></li>
+        <li><a href="#">Dc power visa</a></li> -->
       </ul>
     </div>
   </section>
 </template>
 
 <script>
+export default {
+  name: "Data",
+  data() {
+    return {
+      links: [
+        {
+          text: "digital comics",
+          icon: "../src/assets/img/buy-comics-digital-comics.png",
+          href: "#",
+        },
+        {
+          text: "dc merchandise",
+          icon: "../src/assets/img/buy-comics-merchandise.png",
+          href: "#",
+        },
+        {
+          text: "subscription",
+          icon: "../src/assets/img/buy-comics-subscriptions.png",
+          href: "#",
+        },
+        {
+          text: "comic shop locator",
+          icon: "../src/assets/img/buy-comics-shop-locator.png",
+          href: "#",
+        },
+        {
+          text: "dc power visa",
+          icon: "../src/assets/img/buy-dc-power-visa.svg",
+          href: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
